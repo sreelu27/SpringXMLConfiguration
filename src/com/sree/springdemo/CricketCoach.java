4fb11029b0563f44310eecf7493142a4393d757a
sgeetha2 <sgeetha2@its.jnj.com>
2020-03-18 12:17:03 +0000

    inject literal values

--- src/com/sree/springdemo/CricketCoach.java
+++ src/com/sree/springdemo/CricketCoach.java
@@ -4,10 +4,41 @@ public class CricketCoach implements Coach{
 	
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	private String team;
+	
 	public CricketCoach() {
 		System.out.println("No-arg constructor");
 	}
 	
+	
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+
+	public String getTeam() {
+		return team;
+	}
+
+
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+		System.out.println("Setter method: email");
+	}
+
+
+
+	public void setTeam(String team) {
+		this.team = team;
+		System.out.println("Setter method: team");
+	}
+
+
+
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 		System.out.println("Setter method");
