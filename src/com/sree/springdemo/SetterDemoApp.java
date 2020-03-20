6c959da22dd34b6360cf7aba659898be65d75ff4
sgeetha2 <sgeetha2@its.jnj.com>
2020-03-20 15:12:30 +0000

    Bean Scope Singleton and Prototype

--- src/com/sree/springdemo/SetterDemoApp.java
+++ src/com/sree/springdemo/SetterDemoApp.java
@@ -4,20 +4,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SetterDemoApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        
-		CricketCoach theCoach = context.getBean("myCricketCoach",CricketCoach.class);
-		
-		System.out.println(theCoach.getDailyworkout());
-		
-		System.out.println(theCoach.getDailyFortune());
-		System.out.println(theCoach.getEmailAddress());
-		
-		
-		context.close();
-	}
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * 
+	 * ClassPathXmlApplicationContext context = new
+	 * ClassPathXmlApplicationContext("applicationContext.xml");
+	 * 
+	 * CricketCoach theCoach = context.getBean("myCricketCoach",CricketCoach.class);
+	 * 
+	 * System.out.println(theCoach.getDailyworkout());
+	 * 
+	 * System.out.println(theCoach.getDailyFortune());
+	 * System.out.println(theCoach.getEmailAddress());
+	 * 
+	 * 
+	 * context.close(); }
+	 */
 
 }
